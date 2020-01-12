32249d335ef2611be99f49e5b352f3b9c2e42b37
Bartosz Gamza <bartosz.gamza@monterail.com>
2020-01-12 23:01:25 +0100

    steps poc

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <div v-if="!confirmed">
-      <input type="number" v-model="time">
-      <button @click="confirmTime">Confirm</button>
+    <!-- DEMO TIMER -->
+    <div>
+      <div v-if="!confirmed">
+        <input type="number" v-model="time">
+        <button @click="confirmTime">Confirm</button>
+      </div>
+      <div v-if="confirmed">{{ time }}</div>
     </div>
-    <div v-if="confirmed">{{ time }}</div>
+
+    <!-- STEPS POC -->
+    <div>
+      <div v-for="(step, key) in v60" :key="key" :class="{'step--inactive': !step.current}">
+        <p>{{ step.text }}</p>
+        <button @click="stepCompleted(step, key)">Done</button>
+        <div v-if="step.completed">
+          <p>now wait:</p>
+          <div>{{ time }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,12 +28,32 @@ export default {
   data () {
     return {
       time: null,
-      confirmed: false
+      confirmed: false,
+      v60: [
+        {
+          current: true,
+          text: 'Pour in 50g of water.',
+          completed: false,
+          wait: 20,
+          next: 'Pour in 70g of water'
+        },
+        {
+          current: false,
+          text: 'Pour in 70g of water',
+          wait: 20,
+          completed: false,
+          next: 'Pour in 80g of water'
+        }
+      ]
     }
   },
   methods: {
     confirmTime () {
       this.confirmed = true
+      this.countdown()
+    },
+    setTimer (seconds) {
+      this.time = seconds
       this.countdown()
     },
     countdown () {
@@ -29,8 +64,25 @@ export default {
         }, 1000)
       } else {
         this.confirmed = false
+        this.nextStep()
       }
+    },
+    stepCompleted (step, key) {
+      this.v60[key].completed = true
+      this.time = step.wait
+      this.countdown()
+    },
+    nextStep () {
+      const currentStep = this.v60.findIndex(step => step.current)
+      this.v60[currentStep].current = false
+      this.v60[currentStep + 1].current = true
     }
   }
 }
 </script>
+
+<style scoped>
+.step--inactive {
+  opacity: 0.5;
+}
+</style>
