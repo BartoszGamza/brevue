d974f4cd9e11a0e24d88326730824afe44ad7911
Bartosz Gamza <bartosz.gamza@monterail.com>
2020-01-25 16:27:19 +0100

    add V60 class

---
+++ src/recipes/common.js
@@ -0,0 +1,3 @@
+export const COFFEE_AMNT_PER_100 = 6
+
+export const waterCoffeeRatio = (water, coffee = COFFEE_AMNT_PER_100) => water * coffee / 100
---
+++ src/recipes/v60.js
@@ -0,0 +1,53 @@
+export default class V60 {
+  constructor({
+    coffeeAmount,
+    waterAmount
+  }) {
+    this.coffeeAmount = parseInt(coffeeAmount)
+    this.waterAmount = parseInt(waterAmount)
+    this.boiledWater = () => this.waterAmount + 100
+  }
+
+  static icon = '@/assets/icons/v60.svg'
+
+  get ingredients () {
+    return [
+      `${this.coffeeAmount}g of coffee`,
+      `${this.boiledWater}ml of water`,
+      `V60 drip and filter`
+    ]
+  }
+
+  get preparations () {
+    return [
+      `Grind ${this.coffeeAmount}g of coffee, medium-fine coarse`,
+      `Boil at least ${this.boiledWater}ml of water`,
+      `Rinse the filter with boiled water`,
+      `Insert coffee into filter, put on scale`
+    ]
+  }
+
+  get brewingSteps () {
+    const bloomingPour = this.coffeeAmount * 2
+    const secondPour = this.waterAmount * 0.6
+
+    return [
+      {
+        text: `Pour in ${bloomingPour}g of water.`,
+        wait: 35,
+      },
+      {
+        text: `Pour until you reach ${secondPour}g in total.`,
+        buttonText: 'Start pouring',
+        timerText: 'Finish pouring by',
+        wait: 30,
+      },
+      {
+        text: `Pour untill you reach reach ${this.waterAmount}g in total.`,
+        buttonText: 'Start pouring',
+        timerText: 'Finish pouring by',
+        wait: 30,
+      }
+    ]
+  }
+}
